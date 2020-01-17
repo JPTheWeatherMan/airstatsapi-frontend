4a3a50371d7d3db6f16a4e3f075f9a3b3189852d
Jarrett <jarrettphale@gmail.com>
2020-01-17 13:45:28 -0500

    Link component

---
+++ components/Link.js
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+const StyledLink = styled.a`
+	text-decoration: none;
+	width: ${props => props.width};
+	border: 1px #009ddc solid;
+	border-radius: 5px;
+	font-weight: 700;
+	margin: 20px auto;
+	padding: 0.75rem 2rem;
+	color: black;
+  text-align: center;
+	cursor: pointer;
+`;
+
+const Link = (props) =>{
+return <StyledLink href={props.link} width={props.width}>{`${props.text}`}</StyledLink>
+}
+
+export default Link
